8d3d7da63f2a2e2ace9d58561e499e8c8788401b
Reete Shankar Singh <reetejshankar2001@gmail.com>
2025-07-15 10:02:07 +0530

    Add files via upload

---
+++ TASK1/script.js
@@ -0,0 +1,7 @@
+function showAddCartAlert() {
+      alert("Item has been added successfully!");
+    }
+
+    function showBuyNowAlert() {
+    window.location.href = "https://your-payment-page.com";
+  }
